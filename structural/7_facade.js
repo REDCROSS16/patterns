504b220384e1c395e5ba61027852785282909c86
REDCROSS16 <belkill@mail.ru>
2022-04-16 23:36:34 +0300

    decorator, facade

---
+++ structural/7_facade.js
@@ -0,0 +1,42 @@
+class Complaints {
+    constructor () {
+        this.complaints = [];
+    }
+
+    reply (complaint) { }
+
+    add(complaint) {
+        this.complaints.push(complaint);
+        return this.reply(complaint);
+    }
+}
+
+class ProductComplaints extends Complaints {
+    reply ({ id, customer, details}) {
+        return `Product: ${id}: ${customer}(${details})`;
+    }
+}
+class ServiceComplaints extends Complaints {
+    reply ({ id, customer, details}) {
+        return `Service: ${id}: ${customer}(${details})`;
+    }
+}
+
+class ComplainRegistry {
+    register (customer, type, details) {
+        const id = Date.now();
+        let complaint;
+        if (type == 'service') {
+            complaint = new ServiceComplaints();
+        } else {
+            complaint = new ProductComplaints()
+        }
+
+        return complaint.add({id, customer, details});
+    }
+}
+
+const registry = new ComplainRegistry();
+
+console.log(registry.register('Sanya', 'service', 'недоступен'))
+console.log(registry.register('Bigl', 'product', 'где деньги лебовски?'))
