04705d988c491fffdb6e3ba947aa9c1a5a338ccb
REDCROSS16 <belkill@mail.ru>
2022-04-20 23:44:01 +0300

    flyweight

--- structural/8_flyweight.js
+++ structural/8_flyweight.js
@@ -1,3 +1,27 @@
 // класс flyweight
 
-class 
+class Car {
+    constructor(model, price) {
+        this.model = model;
+        this.price = price;
+    }
+}
+
+class CarFactory {
+    constructor() {
+        this.cars = [];
+    }
+
+    create (model, price) {
+        const candidate = this.getCar(model);
+        if (candidate) return candidate;
+        
+        const newCar = new Car(model, price);
+
+        return newCar;
+    }
+
+    getCar(model) {
+        return this.cars.find(car => car.model === model);
+    }
+}
